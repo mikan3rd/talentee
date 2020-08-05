7bade9dbea8bd3d8269e3a6cf3d55e05a5b4cac7
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-08-06 00:46:31 +0900

    chore: 人気動画取得の仮作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from "firebase-functions";
+import { google } from "googleapis";
+
+const YOUTUBE_API_KEY = "";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,4 +9,17 @@ import * as functions from "firebase-functions";
 export const helloWorld = functions.region("asia-northeast1").https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
+});
+
+export const getYoytubePopularChannel = functions.region("asia-northeast1").https.onRequest(async (req, res) => {
+  const service = google.youtube({ version: "v3", auth: YOUTUBE_API_KEY });
+  const response = await service.search.list({
+    part: ["id", "snippet"],
+    type: ["video"],
+    regionCode: "JP",
+    relevanceLanguage: "ja",
+    order: "viewCount",
+    maxResults: 10,
+  });
+  res.send(response.data.items);
 });
