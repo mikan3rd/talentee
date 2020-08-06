34d9ae1ac67ef903b0548ec866b044adbb6be164
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-08-07 00:38:32 +0900

    chore: firestoreへのデータ保存処理の実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,84 +1,15 @@
 import * as functions from "firebase-functions";
-import { google } from "googleapis";
 import * as dayjs from "dayjs";
+import * as admin from "firebase-admin";
 
 import "dayjs/locale/ja";
 dayjs.locale("ja");
 
-const YOUTUBE_API_KEY = "";
+admin.initializeApp();
+
+import { savePopularChannel } from "./savePopularChannel";
 
 export const getYoytubePopularChannel = functions.region("asia-northeast1").https.onRequest(async (req, res) => {
-  const service = google.youtube({ version: "v3", auth: YOUTUBE_API_KEY });
-
-  const publishedAfter = dayjs().subtract(1, "week").toISOString();
-  const searchResponse = await service.search.list({
-    part: ["id", "snippet"],
-    type: ["video"],
-    regionCode: "JP",
-    relevanceLanguage: "ja",
-    order: "viewCount",
-    maxResults: 10,
-    publishedAfter,
-    location: "35.68,139.76", // 東京駅
-    locationRadius: "500km", // 大阪あたりまで,
-    eventType: "completed",
-  });
-
-  const videoIds = searchResponse.data.items.map((item) => item.id.videoId);
-  const videoResponse = await service.videos.list({
-    part: ["id", "snippet", "contentDetails", "statistics", "player"],
-    hl: "ja",
-    id: videoIds,
-  });
-
-  const channnelIds = videoResponse.data.items.map((item) => item.snippet.channelId);
-  const uniqueChannelIds = Array.from(new Set(channnelIds));
-
-  const channelResponse = await service.channels.list({
-    part: [
-      "id",
-      "snippet",
-      "contentDetails",
-      "statistics",
-      "topicDetails",
-      "brandingSettings",
-      "status",
-      "localizations",
-    ],
-    id: uniqueChannelIds,
-  });
-
-  const result = channelResponse.data.items.map((item) => {
-    const {
-      id,
-      snippet: { title, description, publishedAt, country },
-      statistics: { viewCount, subscriberCount },
-    } = item;
-    const videos = videoResponse.data.items
-      .filter((videoItem) => videoItem.snippet.channelId === id)
-      .map((videoItem) => {
-        const {
-          id,
-          snippet: { title, description, publishedAt, tags, defaultLanguage, defaultAudioLanguage, categoryId },
-          statistics: { viewCount, likeCount, dislikeCount, commentCount },
-        } = videoItem;
-        const videoData = {
-          id,
-          snippet: { title, description, publishedAt, tags, defaultLanguage, defaultAudioLanguage, categoryId },
-          statistics: { viewCount, likeCount, dislikeCount, commentCount },
-        };
-        return videoData;
-      });
-    const data = {
-      channnel: {
-        id,
-        snippet: { title, description, publishedAt, country },
-        statistics: { viewCount, subscriberCount },
-      },
-      videos,
-    };
-    return data;
-  });
-
+  const result = await savePopularChannel();
   res.send({ result });
 });
---
+++ functions/src/savePopularChannel.ts
@@ -0,0 +1,63 @@
+import * as admin from "firebase-admin";
+import { google } from "googleapis";
+import * as dayjs from "dayjs";
+
+const YOUTUBE_API_KEY = "";
+
+const YoutubeChannelCollectionPath = "youtubeChannel" as const;
+
+const { FieldValue } = admin.firestore;
+
+export const savePopularChannel = async () => {
+  const service = google.youtube({ version: "v3", auth: YOUTUBE_API_KEY });
+
+  const publishedAfter = dayjs().subtract(1, "week").toISOString();
+  const searchResponse = await service.search.list({
+    part: ["id", "snippet"],
+    type: ["video"],
+    regionCode: "JP",
+    relevanceLanguage: "ja",
+    order: "viewCount",
+    maxResults: 50,
+    publishedAfter,
+    location: "35.68,139.76", // 東京駅
+    locationRadius: "500km", // 大阪あたりまで,
+  });
+
+  const channnelIds = searchResponse.data.items.map((item) => item.snippet.channelId);
+  const uniqueChannelIds = Array.from(new Set(channnelIds));
+
+  const channelResponse = await service.channels.list({
+    part: ["id", "snippet", "contentDetails", "statistics", "topicDetails", "brandingSettings"],
+    id: uniqueChannelIds,
+  });
+
+  const db = admin.firestore();
+  db.settings({ ignoreUndefinedProperties: true });
+  const youtubeChannelCollection = db.collection(YoutubeChannelCollectionPath);
+
+  const result = [];
+  for (const item of channelResponse.data.items) {
+    const { id, snippet, statistics, brandingSettings } = item;
+
+    const data = {
+      id,
+      snippet,
+      statistics,
+      brandingSettings,
+      createdAt: FieldValue.serverTimestamp(),
+      updatedAt: FieldValue.serverTimestamp(),
+    };
+
+    const ref = youtubeChannelCollection.doc(id);
+    const doc = await ref.get();
+    if (doc.exists) {
+      delete data.createdAt;
+    }
+    await ref.set(data, { merge: true });
+
+    result.push(data);
+  }
+
+  return result;
+};
