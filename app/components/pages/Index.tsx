ef15b219e2de7cf71547c0e691f777080ea3f42e
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-08-10 16:47:33 +0900

    chore: 仮のトップページのスタイル作成

--- app/components/pages/Index.tsx
+++ app/components/pages/Index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Link from "next/link";
 import { css } from "@emotion/core";
-import { Grid, Header, Image, Label, Segment } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 
 import { useIndexData } from "../../hooks/useIndexData";
 
@@ -13,10 +14,73 @@ export const Index: React.FC = () => {
         youtubeData.map((data) => {
           const {
             id,
-            snippet: { title },
+            accountRef,
+            snippet: { title, thumbnails },
             statistics: { subscriberCount },
           } = data;
-          return <div key={id}>{title}</div>;
+          return (
+            <div
+              key={id}
+              css={css`
+                position: relative;
+              `}
+            >
+              <Link href={`/account/${accountRef.id}`} passHref>
+                <a
+                  css={css`
+                    display: flex;
+                    margin-top: 20px;
+                    border-radius: 5px;
+                    padding: 20px;
+                    color: inherit;
+                    background-color: white;
+                    &:hover {
+                      color: inherit;
+                    }
+                  `}
+                >
+                  <div
+                    css={css`
+                      margin-right: 20px;
+                    `}
+                  >
+                    <img
+                      src={thumbnails.medium.url}
+                      alt={title}
+                      css={css`
+                        width: 80px;
+                        height: 80px;
+                        border-radius: 50%;
+                      `}
+                    />
+                  </div>
+                  <div>
+                    <div
+                      css={css`
+                        font-weight: bold;
+                      `}
+                    >
+                      {title}
+                    </div>
+                  </div>
+                </a>
+              </Link>
+
+              <Button
+                color="youtube"
+                as="a"
+                href={`https://www.youtube.com/channel/${id}`}
+                target="_black"
+                css={css`
+                  position: absolute;
+                  bottom: 20px;
+                  right: 20px;
+                `}
+              >
+                <Icon name="youtube" /> YouTube
+              </Button>
+            </div>
+          );
         })}
     </>
   );
--- app/hooks/useIndexData.ts
+++ app/hooks/useIndexData.ts
@@ -5,20 +5,21 @@ import { YoutubeChannelCollectionPath } from "../firebase/firestore";
 
 interface IYoutubeData {
   id: string;
-  snippet: { title; description };
+  snippet: { title: string; description: string; thumbnails: { medium: { url: string } } };
   statistics: { subscriberCount: number };
+  accountRef: { id: string };
 }
 
 export const useIndexData = () => {
-  const [youtubeData, setYoutubeData] = React.useState<firebase.firestore.DocumentData | null>(null);
+  const [youtubeData, setYoutubeData] = React.useState<IYoutubeData[] | null>(null);
 
   const getYoutubePageData = async () => {
     const db = firebase.firestore();
     const youtubeCollection = db.collection(YoutubeChannelCollectionPath);
     const youtubeDocs = await youtubeCollection.orderBy("statistics.subscriberCount", "desc").limit(5).get();
-    const youtubeData: firebase.firestore.DocumentData[] = [];
+    const youtubeData: IYoutubeData[] = [];
     youtubeDocs.forEach((doc) => {
-      const data = doc.data();
+      const data = doc.data() as IYoutubeData;
       youtubeData.push(data);
     });
     setYoutubeData(youtubeData);
