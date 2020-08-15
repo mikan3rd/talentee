aaf6ae58a83b01410e30dfbb6f569341095033b6
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-08-15 20:43:30 +0900

    chore: 月別の取得の追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,10 +19,28 @@ export const getYoutubePopularChannelWeekly = functions
     return result;
   });
 
+export const getYoutubePopularChannelMonthly = functions
+  .region("asia-northeast1")
+  .pubsub.schedule("0 1 * * *")
+  .timeZone("Asia/Tokyo")
+  .onRun(async (context) => {
+    const publishedAfter = dayjs().subtract(1, "month");
+    const result = await savePopularChannel(publishedAfter);
+    return result;
+  });
+
 export const getYoutubePopularChannelWeeklyTest = functions
   .region("asia-northeast1")
   .https.onRequest(async (req, res) => {
     const publishedAfter = dayjs().subtract(1, "week");
+    const result = await savePopularChannel(publishedAfter);
+    res.send({ result });
+  });
+
+export const getYoutubePopularChannelMonthlyTest = functions
+  .region("asia-northeast1")
+  .https.onRequest(async (req, res) => {
+    const publishedAfter = dayjs().subtract(1, "month");
     const result = await savePopularChannel(publishedAfter);
     res.send({ result });
   });
