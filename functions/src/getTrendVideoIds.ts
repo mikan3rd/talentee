2e91456fbc4090960ffa9891cfd2e701060629b8
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-08-18 01:35:51 +0900

    chore: puppeteer-extraの追加

--- functions/src/getTrendVideoIds.ts
+++ functions/src/getTrendVideoIds.ts
@@ -1,12 +1,41 @@
-import * as puppeteer from "puppeteer";
+import * as functions from "firebase-functions";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+import { puppeteerOptions } from "./common/utils";
+
+const GMAIL_ENV = functions.config().gmail;
 
 export const getTrendVideoIds = async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    devtools: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
+  const browser = await puppeteer.use(StealthPlugin()).launch(puppeteerOptions);
   const page = await browser.newPage();
+
+  // await page.setUserAgent(
+  //   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+  // );
+
+  // await page.setExtraHTTPHeaders({
+  //   "Accept-Language": "ja-JP",
+  // });
+
+  const topUrl = `https://www.youtube.com`;
+  await page.goto(topUrl);
+  const LoginSelector = "#masthead-container #buttons ytd-button-renderer a" as const;
+  await page.waitForSelector(LoginSelector, { timeout: 1000 * 60 });
+  await page.click(LoginSelector);
+
+  const MailSelector = "input[type=email]" as const;
+  await page.waitForSelector(MailSelector);
+  await page.type(MailSelector, GMAIL_ENV.email);
+  await page.keyboard.press("Enter");
+
+  const PasswordSelector = "input[type=password]" as const;
+  await page.waitFor(1000 * 5);
+  await page.waitForSelector(PasswordSelector);
+  await page.type(PasswordSelector, GMAIL_ENV.pass);
+  await page.keyboard.press("Enter");
+
+  await page.waitForSelector("div#start");
 
   const trendUrl = `https://www.youtube.com/feed/trending`;
   await page.goto(trendUrl);
@@ -46,6 +75,6 @@ export const getTrendVideoIds = async () => {
   await browser.close();
 
   const uniqueVideoIds = Array.from(new Set(videoIds));
-
+  console.log("trendVideoIds:", uniqueVideoIds);
   return uniqueVideoIds;
 };
--- functions/src/updateRecentVideo.ts
+++ functions/src/updateRecentVideo.ts
@@ -1,17 +1,18 @@
 import * as admin from "firebase-admin";
 import * as dayjs from "dayjs";
+import { PubSub } from "@google-cloud/pubsub";
 
 import { YoutubeChannelCollectionPath } from "./firebase/collectionPath";
 import { getVideoCategories } from "./common/getVideoCategories";
-import { saveChannelPopularVideo } from "./common/saveChannelPopularVideo";
+import { PopularVideoJsonType, PopularVideoTopic } from "./firebase/topic";
 
-export const savePopularVideo = async () => {
+export const updateRecentVideo = async () => {
   const db = admin.firestore();
   const youtubeChannelCollection = db.collection(YoutubeChannelCollectionPath);
 
   const now = dayjs().add(9, "hour");
   const updatedAt = now.subtract(1, "day");
-  const docs = await youtubeChannelCollection.where("updatedAt", ">=", updatedAt).get();
+  const docs = await youtubeChannelCollection.where("updatedAt", ">=", updatedAt.toDate()).get();
 
   const channelDataArray: FirebaseFirestore.DocumentData[] = [];
   docs.forEach((doc) => {
@@ -19,17 +20,15 @@ export const savePopularVideo = async () => {
     channelDataArray.push(data);
   });
 
+  console.log("channelDataArray:", channelDataArray.length);
+
   const videoCategories = await getVideoCategories();
 
+  const pubSub = new PubSub();
   for (const channel of channelDataArray) {
-    const { uniqueVideoCategoryIds, uniqueVideoCategories } = await saveChannelPopularVideo(
-      channel.id,
-      videoCategories,
-    );
-    const channnelData = {
-      videoCategoryIds: uniqueVideoCategoryIds,
-      videoCategories: uniqueVideoCategories,
-    };
-    youtubeChannelCollection.doc(channel.id).set(channnelData, { merge: true });
+    const data: PopularVideoJsonType = { channel, videoCategories };
+    const dataJson = JSON.stringify(data);
+    const dataBuffer = Buffer.from(dataJson);
+    await pubSub.topic(PopularVideoTopic).publish(dataBuffer);
   }
 };
