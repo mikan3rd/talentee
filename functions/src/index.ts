f501e0338699e692be55bf404150f1abb8e476b0
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-08-27 19:48:12 +0900

    chore: functionsにSentry追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,17 @@
-import * as dayjs from "dayjs";
 import * as admin from "firebase-admin";
-
+import * as functions from "firebase-functions";
+import * as dayjs from "dayjs";
+import * as Sentry from "@sentry/node";
 import "dayjs/locale/ja";
-dayjs.locale("ja");
+
+const { dsn, environment } = functions.config().sentry;
+
+Sentry.init({ dsn, environment });
 
 admin.initializeApp();
 const db = admin.firestore();
 db.settings({ ignoreUndefinedProperties: true });
+
+dayjs.locale("ja");
 
 export * from "./youtubeFunc";
