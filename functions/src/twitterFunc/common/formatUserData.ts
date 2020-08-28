c0acf96b9beae9fd323a5a289eba2f832aac8c9c
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-08-29 00:43:34 +0900

    chore: profile_image_urlの整形

--- functions/src/twitterFunc/common/formatUserData.ts
+++ functions/src/twitterFunc/common/formatUserData.ts
@@ -3,5 +3,10 @@ import * as dayjs from "dayjs";
 import { UserDataType, UserObjectType } from "./api";
 
 export const formatTwitterUserData = (userObject: UserObjectType): UserDataType => {
-  return { ...userObject, created_at: dayjs(userObject.created_at).toDate() };
+  const { created_at, profile_image_url } = userObject;
+  return {
+    ...userObject,
+    profile_image_url: profile_image_url.replace(/_normal(?=.jpg$)/, ""),
+    created_at: dayjs(created_at).toDate(),
+  };
 };
