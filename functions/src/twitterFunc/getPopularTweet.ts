8491f171eff7f8fe26041252bcfa98a752b0b993
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-09-02 15:10:21 +0900

    chore: ツイート取得処理を追加

--- functions/src/twitterFunc/getPopularTweet.ts
+++ functions/src/twitterFunc/getPopularTweet.ts
@@ -1,10 +1,13 @@
 import { crawlSearchTweet } from "./common/crawlSearchTweet";
 import { getTweetIdByUrl } from "./common/getTweetIdByUrl";
+import { getTweets } from "./common/api";
 
 export const getPopularTweet = async () => {
   const username = "monst_mixi";
   const linkUrls = await crawlSearchTweet(username);
-  console.log(linkUrls);
   const tweetIds = linkUrls.map((url) => getTweetIdByUrl(url));
-  console.log(tweetIds);
+  console.log(JSON.stringify({ tweetIds }));
+
+  const { data } = await getTweets(tweetIds);
+  console.log(data);
 };
