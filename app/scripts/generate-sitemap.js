3f4dde0239d5537aa223314bb0e60ce6cdfe5a01
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-09-02 23:00:35 +0900

    chore: sitemapにアカウントページ追加

--- app/scripts/generate-sitemap.js
+++ app/scripts/generate-sitemap.js
@@ -1,9 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require("fs");
 
+require("dotenv").config({ path: ".env.local" });
+
+const admin = require("firebase-admin");
 const globby = require("globby");
 const prettier = require("prettier");
 const dayjs = require("dayjs");
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
 
 const baseUrl = "https://talentee.vercel.app";
 const siteUpdatedAt = dayjs().format("YYYY-MM-DD");
@@ -12,8 +24,21 @@ const siteUpdatedAt = dayjs().format("YYYY-MM-DD");
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby(["pages/**/*.tsx", "!pages/_*.tsx", "!pages/account"]);
 
+  const db = admin.firestore();
+  const youtubeDocs = await db
+    .collection("youtubeChannel")
+    .orderBy("statistics.subscriberCount", "desc")
+    .limit(100)
+    .get();
+  const accountPages = [];
+  youtubeDocs.forEach((doc) => {
+    const { accountRef } = doc.data();
+    const accountPath = `/account/${accountRef.id}`;
+    accountPages.push(accountPath);
+  });
+
   // Combine them into the pages you care about
-  const allPages = [...pages];
+  const allPages = [...pages, ...accountPages];
 
   const sitemap = `
 <?xml version="1.0" encoding="UTF-8"?>
