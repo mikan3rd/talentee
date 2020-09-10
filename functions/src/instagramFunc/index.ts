35d8a48ec7a52cfede7966b89491f1871fa119b9
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-09-10 18:23:09 +0900

    chore: InstagramのPubSubをコメントアウト

--- functions/src/instagramFunc/index.ts
+++ functions/src/instagramFunc/index.ts
@@ -1,18 +1,18 @@
 import { sentryWrapper } from "../common/sentry";
 import { functions } from "../firebase/functions";
-import { UpsertInstagramUserJsonType, UpsertInstagramUserTopic } from "../firebase/topic";
+// import { UpsertInstagramUserJsonType, UpsertInstagramUserTopic } from "../firebase/topic";
 
 import { upsertProfile } from "./upsertProfile";
 
-export const upsertProfilePubSub = functions
-  .runWith({ timeoutSeconds: 540, memory: "2GB", maxInstances: 10 })
-  .pubsub.topic(UpsertInstagramUserTopic)
-  .onPublish(
-    sentryWrapper(async (message) => {
-      const { accountId, username } = message.json as UpsertInstagramUserJsonType;
-      return await upsertProfile(accountId, username);
-    }),
-  );
+// export const upsertProfilePubSub = functions
+//   .runWith({ timeoutSeconds: 540, memory: "2GB", maxInstances: 10 })
+//   .pubsub.topic(UpsertInstagramUserTopic)
+//   .onPublish(
+//     sentryWrapper(async (message) => {
+//       const { accountId, username } = message.json as UpsertInstagramUserJsonType;
+//       return await upsertProfile(accountId, username);
+//     }),
+//   );
 
 export const upsertProfileTest = functions.runWith({ timeoutSeconds: 540, memory: "2GB" }).https.onRequest(
   sentryWrapper(async (req, res) => {
