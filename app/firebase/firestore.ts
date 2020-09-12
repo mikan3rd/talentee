e7f4144a79ba251226cb8b3cdd3c5a9932777b34
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-09-12 19:04:47 +0900

    chore: トップページにInstagramの導線追加

--- app/firebase/firestore.ts
+++ app/firebase/firestore.ts
@@ -5,3 +5,5 @@ export const YoutubeVideoCollectionPath = "youtubeVideo" as const;
 
 export const TwitterUserCollectionPath = "twitterUser" as const;
 export const TwitterTweetCollectionPath = "twitterTweet" as const;
+
+export const InstagramUserCollectionPath = "instagramUser" as const;
