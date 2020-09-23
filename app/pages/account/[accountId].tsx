ac42e146782d11ff22d88aedcd1ac0cd9e4176ee
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-09-23 12:54:08 +0900

    chore: 404エラー対応

--- app/pages/account/[accountId].tsx
+++ app/pages/account/[accountId].tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetServerSidePropsResult } from "next";
 import Router from "next/router";
 import Link from "next/link";
+import Error from "next/error";
 import { Breadcrumb, Divider } from "semantic-ui-react";
 
 import { getAccountPageData } from "../../fetchData/accountPageData";
@@ -9,75 +10,86 @@ import { Account } from "../../components/pages/Account";
 import { Meta } from "../../components/templates/Meta";
 import { AccountSection, TopSection } from "../../components/templates/BreadcrumbSection";
 
-const ProfilePage = React.memo<{ data: { accountId: string; jsonData: string } }>(
-  ({ data: { accountId, jsonData } }) => {
-    const {
-      accountData,
-      youtubeData,
-      youtubePopularVideos,
-      twitterUserData,
-      popularTweets,
-      instagramUserData,
-      instagramPopularMedia,
-      tiktokUserData,
-      tiktokPopularItem,
-    }: {
-      accountData: AccountDataType;
-      youtubeData?: IYoutubeData;
-      youtubePopularVideos: IYoutubeVideoData[];
-      twitterUserData?: TwitterUserDataType;
-      popularTweets: TweetDataType[];
-      instagramUserData: InstagramUserDataType;
-      instagramPopularMedia: InstagramMediaType[];
-      tiktokUserData: TiktokUserDataType;
-      tiktokPopularItem: TiktokItemType[];
-    } = JSON.parse(jsonData);
+type Props = { data: { accountId: string; jsonData: string }; statusCode: number };
 
-    if (!accountData) {
-      Router.push("/");
-      return null;
-    }
+const ProfilePage = React.memo<Props>(({ data: { accountId, jsonData }, statusCode }) => {
+  if (statusCode !== 200) {
+    return <Error statusCode={statusCode} />;
+  }
 
-    const { tmpUsername } = accountData;
+  const {
+    accountData,
+    youtubeData,
+    youtubePopularVideos,
+    twitterUserData,
+    popularTweets,
+    instagramUserData,
+    instagramPopularMedia,
+    tiktokUserData,
+    tiktokPopularItem,
+  }: {
+    accountData: AccountDataType;
+    youtubeData?: IYoutubeData;
+    youtubePopularVideos: IYoutubeVideoData[];
+    twitterUserData?: TwitterUserDataType;
+    popularTweets: TweetDataType[];
+    instagramUserData: InstagramUserDataType;
+    instagramPopularMedia: InstagramMediaType[];
+    tiktokUserData: TiktokUserDataType;
+    tiktokPopularItem: TiktokItemType[];
+  } = JSON.parse(jsonData);
 
-    return (
-      <>
-        <Meta title={tmpUsername} description={`${tmpUsername} のまとめページはコチラ！`} />
+  if (!accountData) {
+    Router.push("/");
+    return null;
+  }
 
-        <Breadcrumb size="big">
-          <TopSection />
-          <Breadcrumb.Divider />
-          <AccountSection />
-          <Breadcrumb.Divider />
-          <Link href={`/account/${accountId}`}>
-            <Breadcrumb.Section href={`/account/${accountId}`} active={true}>
-              {accountData.tmpUsername}
-            </Breadcrumb.Section>
-          </Link>
-        </Breadcrumb>
+  const { tmpUsername } = accountData;
 
-        <Divider />
+  return (
+    <>
+      <Meta title={tmpUsername} description={`${tmpUsername} のまとめページはコチラ！`} />
 
-        <Account
-          accountData={accountData}
-          youtubeData={youtubeData}
-          youtubePopularVideos={youtubePopularVideos}
-          twitterUserData={twitterUserData}
-          popularTweets={popularTweets}
-          instagramUserData={instagramUserData}
-          instagramPopularMedia={instagramPopularMedia}
-          tiktokUserData={tiktokUserData}
-          tiktokPopularItem={tiktokPopularItem}
-        />
-      </>
-    );
-  },
-);
+      <Breadcrumb size="big">
+        <TopSection />
+        <Breadcrumb.Divider />
+        <AccountSection />
+        <Breadcrumb.Divider />
+        <Link href={`/account/${accountId}`}>
+          <Breadcrumb.Section href={`/account/${accountId}`} active={true}>
+            {accountData.tmpUsername}
+          </Breadcrumb.Section>
+        </Link>
+      </Breadcrumb>
+
+      <Divider />
+
+      <Account
+        accountData={accountData}
+        youtubeData={youtubeData}
+        youtubePopularVideos={youtubePopularVideos}
+        twitterUserData={twitterUserData}
+        popularTweets={popularTweets}
+        instagramUserData={instagramUserData}
+        instagramPopularMedia={instagramPopularMedia}
+        tiktokUserData={tiktokUserData}
+        tiktokPopularItem={tiktokPopularItem}
+      />
+    </>
+  );
+});
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const accountId = params.accountId as string;
   const jsonData = await getAccountPageData(accountId);
-  return { props: { data: { accountId, jsonData } } };
+
+  let statusCode = 200;
+  if (jsonData === null) {
+    statusCode = 404;
+  }
+
+  const result: GetServerSidePropsResult<Props> = { props: { data: { accountId, jsonData }, statusCode } };
+  return result;
 };
 
 export default ProfilePage;
