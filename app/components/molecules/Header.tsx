ec3830d801239809d12b515a86b1a372f2f800af
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-09-30 17:27:03 +0900

    chore: ヘッダーに検索ページ遷移ボタン追加

--- app/components/molecules/Header.tsx
+++ app/components/molecules/Header.tsx
@@ -2,46 +2,60 @@ import React from "react";
 
 import { css } from "@emotion/core";
 import Link from "next/link";
-import { Image } from "semantic-ui-react";
+import { Button, Container, Icon, Image } from "semantic-ui-react";
 
 import { useScrollDirection } from "../../hooks/useScrollDirection";
 
 export const Header = React.memo(() => {
   const isUp = useScrollDirection();
   return (
-    <Link href="/" passHref>
-      <header
-        id="header"
+    <header
+      id="header"
+      css={css`
+        position: fixed;
+        top: ${!isUp ? "-60px" : "0"};
+        left: 0;
+        right: 0;
+        height: 60px;
+        background-color: black;
+        z-index: 1;
+        transition: all 0.5s ease;
+      `}
+    >
+      <Container
+        text
         css={css`
-          position: fixed;
-          top: ${!isUp ? "-60px" : "0"};
-          left: 0;
-          right: 0;
-          height: 60px;
-          background-color: black;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          z-index: 1;
-          transition: all 0.5s ease;
-          cursor: pointer;
+          &&& {
+            height: 60px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+          }
         `}
       >
-        <div
-          css={css`
-            height: 80%;
-            margin-left: 10px;
-          `}
-        >
-          <Image
-            src="/logo_header.png"
-            alt="Talentee"
+        <Link href="/" passHref>
+          <a
             css={css`
-              height: 100%;
+              height: 80%;
             `}
-          />
-        </div>
-      </header>
-    </Link>
+          >
+            <Image
+              src="/logo_header.png"
+              alt="Talentee"
+              css={css`
+                &&& {
+                  height: 100%;
+                }
+              `}
+            />
+          </a>
+        </Link>
+        <Link href="/search" passHref>
+          <Button icon inverted as="a">
+            <Icon name="search" size="large" />
+          </Button>
+        </Link>
+      </Container>
+    </header>
   );
 });
