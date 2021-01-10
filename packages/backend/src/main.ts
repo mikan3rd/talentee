80c269b0365bff14215522d9bdbe6c2df5513f68
mikan3rd <mikan.no.kikoushi@gmail.com>
2021-01-10 18:23:17 +0900

    chore: add morgan

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from "@nestjs/core";
+import * as morgan from "morgan";
 
-import { AppModule } from "./modules/app.module";
+import { AppModule } from "@/modules/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan("dev"));
   app.enableCors();
   await app.listen(process.env.PORT || 3300);
 }
