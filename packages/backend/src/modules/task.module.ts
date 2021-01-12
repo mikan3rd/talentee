41578b2ebd4a4043264b1884c4a20160e19d53e5
mikan3rd <mikan.no.kikoushi@gmail.com>
2021-01-13 01:16:00 +0900

    chore: add saveYoutubeVideoCategories

--- packages/backend/src/modules/task.module.ts
+++ packages/backend/src/modules/task.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
 
+import { YoutubeModule } from "@/modules/youtube.module";
+import { AccountService } from "@/services/account.service";
 import { TaskService } from "@/services/task.service";
+import { YoutubeService } from "@/services/youtube.service";
 
 @Module({
+  imports: [YoutubeModule],
   providers: [TaskService],
+  exports: [TaskService],
 })
 export class TaskModule {}
--- packages/backend/src/services/task.service.ts
+++ packages/backend/src/services/task.service.ts
@@ -1,12 +1,23 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { Cron } from "@nestjs/schedule";
+import { Cron, Timeout } from "@nestjs/schedule";
+
+import { YoutubeService } from "@/services/youtube.service";
 
 @Injectable()
 export class TaskService {
   private readonly logger = new Logger(TaskService.name);
 
+  constructor(private youtubeService: YoutubeService) {}
+
   @Cron("0 * * * * *")
-  handleCron() {
+  async handleCron() {
     this.logger.debug("Called when the second is 0");
+  }
+
+  @Timeout(1000)
+  async saveYoutubeVideoCategories() {
+    this.logger.debug("START: saveYoutubeVideoCategories");
+    await this.youtubeService.saveVideoCategories();
+    this.logger.debug("END: saveYoutubeVideoCategories");
   }
 }
