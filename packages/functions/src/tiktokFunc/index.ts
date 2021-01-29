fda90e6c1b1e00e03883fca583b8fcfef4cb93c2
mikan3rd <mikan.no.kikoushi@gmail.com>
2021-01-29 20:11:19 +0900

    fix: comment out cloud function

--- packages/functions/src/tiktokFunc/index.ts
+++ packages/functions/src/tiktokFunc/index.ts
@@ -4,15 +4,15 @@ import { Topic, UpsertTiktokUserJsonType } from "../firebase/topic";
 
 import { upsertUserData } from "./upsertUserData";
 
-// export const upsertTiktokUserDataPubSub = functions
-//   .runWith({ memory: "1GB", maxInstances: 10 })
-//   .pubsub.topic(Topic.UpsertTiktokUser)
-//   .onPublish(
-//     sentryWrapper(async (message) => {
-//       const { accountId, uniqueId } = message.json as UpsertTiktokUserJsonType;
-//       return await upsertUserData(accountId, uniqueId);
-//     }),
-//   );
+export const upsertTiktokUserDataPubSub = functions
+  .runWith({ memory: "1GB", maxInstances: 10 })
+  .pubsub.topic(Topic.UpsertTiktokUser)
+  .onPublish(
+    sentryWrapper(async (message) => {
+      const { accountId, uniqueId } = message.json as UpsertTiktokUserJsonType;
+      return await upsertUserData(accountId, uniqueId);
+    }),
+  );
 
 export const upsertTiktokUserDataTest = functions.runWith({ memory: "1GB" }).https.onRequest(
   sentryWrapper(async (req, res) => {
