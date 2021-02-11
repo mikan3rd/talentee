46328806a330e314fb06068ee6ed2f55b16e0119
mikan3rd <mikan.no.kikoushi@gmail.com>
2021-02-11 13:14:59 +0900

    fix: delete typeorm

--- packages/backend/src/models/account.model.ts
+++ packages/backend/src/models/account.model.ts
@@ -1,43 +1,33 @@
 import { Field, ID, ObjectType } from "@nestjs/graphql";
-import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 import { InstagramUser } from "@/models/instagramUser.model";
 import { TiktokUser } from "@/models/tiktokUser.model";
 import { TwitterUser } from "@/models/twitterUser.model";
 import { YoutubeChannel } from "@/models/youtubeChannel.model";
 
-@Entity("Account")
 @ObjectType()
 export class Account {
-  @PrimaryGeneratedColumn("uuid")
   @Field((type) => ID)
   uuid!: string;
 
-  @Column()
   @Field()
   displayName!: string; // 表示用の名前
 
-  @Column({ unique: true })
   @Field()
   username!: string; // URLなどに使用する半角英数字
 
-  @Column({ type: "text" })
   @Field()
   thumbnailUrl!: string;
 
-  @CreateDateColumn()
   @Field()
   createdAt!: Date;
 
-  @UpdateDateColumn()
   @Field()
   updatedAt!: Date;
 
-  @OneToMany((type) => YoutubeChannel, (channel) => channel.account)
   @Field((type) => [YoutubeChannel])
   youtubeChannels!: YoutubeChannel[];
 
-  @OneToMany((type) => TwitterUser, (user) => user.account)
   @Field((type) => [TwitterUser])
   twitterUsers!: TwitterUser[];
 
--- packages/backend/src/models/youtubeVideo.model.ts
+++ packages/backend/src/models/youtubeVideo.model.ts
@@ -1,74 +1,51 @@
 import { Field, ObjectType } from "@nestjs/graphql";
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
 
 import { YoutubeChannel } from "@/models/youtubeChannel.model";
 import { YoutubeVideoCategory } from "@/models/youtubeVideoCategoriy.model";
 import { YoutubeVideoTagRelation } from "@/models/youtubeVideoTagRelation.mode";
 import { BigIntScalar } from "@/scalars/bigint.scalar";
 
-@Entity("YoutubeVideo")
 @ObjectType()
 export class YoutubeVideo {
-  @PrimaryColumn()
   @Field()
   id!: string;
 
-  @Column()
   @Field()
   title!: string;
 
-  @Column({ type: "text" })
   @Field()
   description!: string;
 
-  @Column({ type: "text" })
   @Field()
   thumbnailUrl!: string;
 
-  @Column({ type: "datetime" })
   @Field()
   publishedAt!: Date;
 
-  @Column({ type: "bigint", unsigned: true, nullable: true, default: null })
   @Field((type) => BigIntScalar, { nullable: true })
   viewCount?: BigInt;
 
-  @Column({ type: "bigint", unsigned: true, nullable: true, default: null })
   @Field((type) => BigIntScalar, { nullable: true })
   likeCount?: BigInt;
 
-  @Column({ type: "bigint", unsigned: true, nullable: true, default: null })
   @Field((type) => BigIntScalar, { nullable: true })
   dislikeCount?: BigInt;
 
-  @Column({ type: "bigint", unsigned: true, nullable: true, default: null })
   @Field((type) => BigIntScalar, { nullable: true })
   commentCount?: BigInt;
 
-  @CreateDateColumn()
   @Field()
   createdAt!: Date;
 
-  @UpdateDateColumn()
   @Field()
   updatedAt!: Date;
 
   @Field((type) => [YoutubeVideoTagRelation])
   tags!: YoutubeVideoTagRelation[];
 
-  @ManyToOne((type) => YoutubeVideoCategory, (category) => category.videos, {
-    nullable: false,
-    onUpdate: "CASCADE",
-  })
-  @JoinColumn({ name: "videoCategoryId" })
   @Field((type) => YoutubeVideoCategory)
   videoCategory!: YoutubeVideoCategory;
 
-  @ManyToOne((type) => YoutubeChannel, (channel) => channel.videos, {
-    nullable: false,
-    onUpdate: "CASCADE",
-  })
-  @JoinColumn({ name: "channelId" })
   @Field((type) => YoutubeChannel)
   channel!: YoutubeChannel;
 }
