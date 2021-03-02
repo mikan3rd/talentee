91a7c31cd25d46e724ce687eaf0a00cd19bec16f
mikan3rd <mikan.no.kikoushi@gmail.com>
2021-03-03 00:06:05 +0900

    chore: addAccount for twitter

--- packages/backend/src/modules/twitter.module.ts
+++ packages/backend/src/modules/twitter.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 
+import { TwitterController } from "@/controllers/twitter.controller";
 import { TwitterResolver } from "@/resolvers/twitter.resolver";
 import { CrawlService } from "@/services/crawl.service";
 import { PrismaService } from "@/services/prisma.service";
@@ -9,6 +10,7 @@ import { UtilsService } from "@/services/utils.service";
 @Module({
   imports: [],
   providers: [TwitterService, CrawlService, UtilsService, PrismaService, TwitterResolver],
+  controllers: [TwitterController],
   exports: [TwitterService],
 })
 export class TwitterModule {}
